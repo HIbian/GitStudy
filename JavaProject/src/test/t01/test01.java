d41be016306e622c183589317b6ed4cf71426182
Hibian <416711267@qq.com>
2018-09-06 20:31:42 +0800

    webmagic框架

---
+++ JavaProject/src/test/t01/test01.java
@@ -0,0 +1,36 @@
+package test.t01;
+
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+public class test01 implements PageProcessor {
+
+    private Site site = Site.me().setRetryTimes(3).setSleepTime(100);
+
+    public void process(Page page) {
+    	
+        page.addTargetRequests(page.getHtml().links().regex("(https://github\\.com/\\w+/\\w+)").all());
+        page.putField("author", page.getUrl().regex("https://github\\.com/(\\w+)/.*").toString());
+        page.putField("name", page.getHtml().xpath("//h1[@class='entry-title public']/strong/a/text()").toString());
+        if (page.getResultItems().get("name")==null){
+            //skip this page
+            page.setSkip(true);
+        }
+        page.putField("readme", page.getHtml().xpath("//div[@id='readme']/tidyText()"));
+    }
+
+    public Site getSite() {
+        return site;
+    }
+
+    public static void main(String[] args) {
+//    	BasicConfigurator.configure(); //自动快速地使用缺省Log4j环境。
+    	String[] kStrings = new String[1];
+    	kStrings[0] = "https://github.com/HIbian";
+        Spider.create(new test01()).addUrl(kStrings).thread(5).run();
+    }
+
+}
