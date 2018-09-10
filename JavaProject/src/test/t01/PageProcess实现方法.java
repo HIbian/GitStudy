f5951189bbbc9365028cd1425f73f151b453f1ed
Hibian <416711267@qq.com>
2018-09-10 23:36:57 +0800

    爬取B站视频标题和时间Demo

--- JavaProject/src/test/t01/PageProcess实现方法.java
+++ JavaProject/src/test/t01/PageProcess实现方法.java
@@ -1,7 +1,5 @@
 package test.t01;
 
-import java.util.List;
-
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
@@ -10,9 +8,10 @@ import us.codecraft.webmagic.processor.PageProcessor;
 public class PageProcess实现方法 implements PageProcessor{
 	private static int count =123422;
 	//设置网站相关配置
-	private Site site = Site.me().setRetryTimes(5).setSleepTime(1000).setUserAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36");//重试次数和抓取间隔
+	private Site site = Site.me().setRetryTimes(5).setSleepTime(0).setUserAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36");//重试次数和抓取间隔
 	public void process(Page page) {
 		System.out.println(page.getHtml().xpath("//h1/span/text()").get());
+		System.out.println(page.getHtml().xpath("//div/time/text()"));
 	}
 	public Site getSite() {
 		return site;
