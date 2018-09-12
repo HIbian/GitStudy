3f971bc723869909ff0a7098f33203229ea0e77b
Hibian <416711267@qq.com>
2018-09-12 08:16:32 +0800

    2018-9-12 爬虫

--- JavaProject/src/test/t01/PageProcess实现方法.java
+++ JavaProject/src/test/t01/PageProcess实现方法.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
 
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
@@ -13,11 +14,20 @@ import us.codecraft.webmagic.processor.PageProcessor;
 public class PageProcess实现方法 implements PageProcessor{
 	private static int count =123422;
 	//设置网站相关配置
-	private Site site = Site.me().setRetryTimes(5).setSleepTime(0).setUserAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36");//重试次数和抓取间隔
+	private Site site = Site.me().setRetryTimes(5).setSleepTime(1).setUserAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36");//重试次数和抓取间隔
 	public void process(Page page) {
 		String title = page.getHtml().xpath("//h1/span/text()").get();
 		String time = page.getHtml().xpath("//div/time/text()").get();
-		intoSql(title, time);
+		List<String> m_url = page.getHtml().links().all();
+		System.out.println(title);
+		System.out.println(time);
+		System.out.println("-----------------------------------------------------");
+
+		for (String string : m_url) {
+			System.out.println(string);
+		}
+		System.out.println("-----------------------------------------------------");
+//		intoSql(title, time);
 	}
 	public Site getSite() {
 		return site;
