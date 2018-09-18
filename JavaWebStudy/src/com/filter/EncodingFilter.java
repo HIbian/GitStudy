04e2d3154a8e3d9ff763fe753eda1275dfcaa92b
Hibian <416711267@qq.com>
2018-09-18 18:50:38 +0800

    2018-09-18 18:49:28 删除多余语句

--- JavaWebStudy/src/com/filter/EncodingFilter.java
+++ JavaWebStudy/src/com/filter/EncodingFilter.java
@@ -15,20 +15,16 @@ import javax.servlet.ServletResponse;
 public class EncodingFilter implements Filter{
 	
 	public EncodingFilter() {
-		System.out.println("过滤器构造");
 	}
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
-		System.out.println("过滤器初始化");
 	}
 	@Override
 	public void destroy() {
-		System.out.println("过滤器销毁");
 	}
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html;charset=utf-8");
 		chain.doFilter(request, response);
